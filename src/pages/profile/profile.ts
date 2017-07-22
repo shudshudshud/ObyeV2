ad719ee113e0dc17238d930b81b16e3491e31c38
Sriram Sami <frizensami@gmail.com>
2017-07-22 16:35:13 +0800

    Add skeleton code for profile page

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GoogleAuth, FacebookAuth, Auth, User, UserSocialProviderDetailsData, UserSocialProviderDetails } from '@ionic/cloud-angular';
 
 @Component({
   selector: 'page-profile',
@@ -7,7 +8,33 @@ import { NavController } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController) {
+  // Shared vars
+  name: String;
+  picture: String;
+
+  constructor(public navCtrl: NavController, public user: User) {
+    //this.user = user;
+    //alert(JSON.stringify(this.user.details));
+    //alert(JSON.stringify(this.user.social.facebook));
+    //alert(JSON.stringify(this.user.social.google));
+    
+    // Find out if we are using Google or Facebook
+    let profile: UserSocialProviderDetails = null;
+
+    if (this.user.social.google != undefined) {
+      profile = this.user.social.google;
+    } else if (this.user.social.facebook != undefined) {
+      profile = this.user.social.facebook;
+    }
+on
+    // Test
+    if (profile == undefined) {
+      this.name = "2 Chainz";
+      this.picture = "https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg"
+    } else {
+      this.name = profile.data.full_name;
+      this.picture = profile.data.profile_picture;
+    }
 
   }
 }
