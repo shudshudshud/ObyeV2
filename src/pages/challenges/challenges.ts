7b5f9f16a41ce222bf77e4c13c3f8991c138cd2d
DESKTOP-TNF01RJ\Irsyad <irsyadramthan@gmail.com>
2017-07-23 15:09:39 +0800

    Added segments to challenges page

--- src/pages/challenges/challenges.ts
+++ src/pages/challenges/challenges.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-challenges',
   templateUrl: 'challenges.html'
+  
 })
 export class ChallengesPage {
-
+  challenges: string = "diet";
   items: FirebaseListObservable<any>;
   constructor(public navCtrl: NavController, db: AngularFireDatabase) {
     this.items = db.list('/streaks');
